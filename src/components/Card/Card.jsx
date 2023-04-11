dcb0d7467ca297b60d71d7af5fe3182660eb0e7c
Tomas Truyols <tomast25@hotmail.com>
2023-04-11 19:56:01 +0200

    feat: Created a working card component that accepts a query from a searchbar and sends a request to api, If request is incorrect, throwing an error handling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Card.scss";
 
-const Card = () => {
+const Card = ({ query }) => {
+  const [weatherData, setWeatherData] = useState({});
+
   const fetchData = async () => {
-    const res = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${
+    try {
+      const path = `https://api.weatherapi.com/v1/current.json?key=${
         import.meta.env.VITE_WEATHER_API_KEY
-      }&q=London&aqi=yes`
-    );
-    console.log(res);
-    const data = await res.json();
-    console.log(data);
+      }`;
+      const res = await fetch(`${path}&q=${query ? query : "London"}&aqi=yes`);
+      if (!res.ok) {
+        throw new Error("Sorry something went wrong !");
+      }
+      const data = await res.json();
+      setWeatherData(data);
+      return data;
+    } catch (e) {
+      console.clear();
+      console.log(e.message);
+    }
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
-  return <div>Card</div>;
+  }, [query]);
+
+  console.log(weatherData);
+
+  return (
+    <div>
+      {Object.keys(weatherData).length > 0 && (
+        <div className="card__data">
+          <h2 className="City">{weatherData.location.name}</h2>
+          <p>{weatherData.location.country}</p>
+          <h3 className="Temperature">{weatherData.current.temp_c}°C</h3>
+          <img src={weatherData.current.condition.icon} alt="weather condition" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Card;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../components/Card/Card";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./Main.scss";
 
 const Main = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setQuery(e.target[0].value);
+  };
+
   return (
     <div>
-      <Card />
+      <SearchBar handleSearch={handleSearch} />
+      <Card query={query} />
     </div>
   );
 };
