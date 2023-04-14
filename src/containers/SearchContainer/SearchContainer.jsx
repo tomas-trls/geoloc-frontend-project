a11ceb4f8a7e6ef1f9b100e9022c72746c756fbc
Tomas Truyols <tomast25@hotmail.com>
2023-04-14 21:10:32 +0200

    fix: fixed spacing on title

--- src/containers/SearchContainer/SearchContainer.jsx
+++ src/containers/SearchContainer/SearchContainer.jsx
@@ -14,8 +14,7 @@ const SearchContainer = () => {
     <div className="search-container">
       <div className="search-container__description">
         <h2 className="search-container__title">
-          No matter the
-          <span className="search-container__title-pink">Weather</span>
+          No matter the <span className="search-container__title-pink">Weather</span>
           <br />
           Become the best version of yourself
         </h2>
